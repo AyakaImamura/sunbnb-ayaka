38c4564325693566306713a00577c94e731dec91
Ayaka Imamura <aykcw.724@gmail.com>
2020-03-06 10:16:18 +0800

    [Layout] Update Devise Layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root 'pages#home'
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions',
+    passwords: 'users/passwords'
+  }
+  # devise_for :users,controllersは、usersフォルダの中にcontrollersがたくさん入っている
+  #'users/passwords'は、usersがフォルダの名前で、passwordsはコントローラの名前
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
