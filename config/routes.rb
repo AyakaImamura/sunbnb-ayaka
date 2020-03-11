4dee61b4c98cdb66a4db4cdaa413681f78f4b80d
AyakaImamura <60721149+AyakaImamura@users.noreply.github.com>
2020-03-11 10:26:47 +0800

    [Layout]User show page (#7)

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     omniauth_callbacks: 'users/omniauth_callbacks'
   }
+
+  resources :users, only: [:show]
+
   # devise_for :users,controllersは、usersフォルダの中にcontrollersがたくさん入っている
   #'users/passwords'は、usersがフォルダの名前で、passwordsはコントローラの名前
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
