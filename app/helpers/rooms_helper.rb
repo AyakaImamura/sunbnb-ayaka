3266f94a571634410002cf6385c683eb8333412e
AyakaImamura <60721149+AyakaImamura@users.noreply.github.com>
2020-03-18 14:44:23 +0800

    [function]publish room (#13)

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,18 @@
 module RoomsHelper
+  def is_done?(item)
+    unless item.blank?
+      content_tag :span, class: "" do
+        content_tag :i, nil, class: "fa fa-check"
+      end
+    end
+  end
+
+  def is_not_ready?(room)
+    room.price.blank? || room.name.blank? || room.photos.blank? || room.address.blank?
+  end
+  # When "is_active? and price etc.." is blank, status of publish button isn't ready 
+
+  # def is_ready?(room)
+  #     !room.is_active? && !room.price.blank? && !room.name.blank? && !room.photos.blank? && !room.address.blank?
+  # end
 end
